f5b2ea56f48d048a93b05948384b607fbb9e391c
SaraReda8 <sara000537@gmail.com>
2025-05-23 21:51:19 +0200

    edits

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true })]
+    }
+  }
 })
